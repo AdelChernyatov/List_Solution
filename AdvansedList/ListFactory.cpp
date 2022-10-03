e5535a0efcd06ce5121ee927050c202dc6461dee
ArturSalamatin <43352868+ArturSalamatin@users.noreply.github.com>
2022-10-03 08:39:19 +0300

    fix: code style improved

--- AdvansedList/ListFactory.cpp
+++ AdvansedList/ListFactory.cpp
@@ -4,36 +4,36 @@ namespace ListSpace
 {
 	List* ListFactory::GenListof1el(const std::string& name, unsigned char mark) // по ссылке на чтение передаем
 	{
-		//ListSpace::List list(name, mark);
-		List* l = new List(name,mark);
+		List* l = new List(name, mark);
+		return l;
+	}
 
-		return l;
-	}
 	List* ListFactory::GenListof2el(const std::string& name1, unsigned char mark1,
-		const std::string& name2, unsigned char mark2) 
+		const std::string& name2, unsigned char mark2)
 	{
-		List* l = new List(name1, mark1);
-		l->next = new List(name2, mark2);
+		List* l{ GenListof1el(name1, mark1) };
+		l->next = GenListof1el(name2, mark2);
 		return l;
 	}
+
 	List* ListFactory::To2elLoop(const std::string& name1, unsigned char mark1,
-		const std::string& name2, unsigned char mark2) 
-	{   
-		List* l = new List(name1, mark1);
-		l->next = new List(name2, mark2);
+		const std::string& name2, unsigned char mark2)
+	{
+		List* l{ GenListof2el(name1, mark1, name2, mark2) };
 		l = ListFactory::SetLoop(l);
 		return l;
 	}
-	 List* ListFactory::SetLoop(List* list)  // изначально нет цикла в списке -> вручную зацикливаем путем присвоения последнему элементу первого (list)
-	 {
-     	 List* head = list;
 
-		 while (head->next != nullptr)
-		 {
-			 head = head->next;
-		 }
-		 head ->next = list;
-		 return list;
-	 }
+	List* ListFactory::SetLoop(List* list)  // изначально нет цикла в списке -> вручную зацикливаем путем присвоения последнему элементу первого (list)
+	{
+		List* head = list;
+
+		while (head->next != nullptr)
+		{
+			head = head->next;
+		}
+		head->next = list;
+		return list;
+	}
 
 } // ListSpace
